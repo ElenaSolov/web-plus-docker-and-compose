6cb717a4dcfbadfabb323d8812c2ce35b8873a58
Elena Soloveva <seaver@yandex.ru>
2023-02-12 15:57:52 +0200

    Feat: docker-compose.yml for db and backend, move .env to the main directory

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,12 +1,12 @@
 export default () => ({
-  port: parseInt(process.env.PORT) || 3000,
+  port: parseInt(process.env.PORT) || 3001,
   db: {
     type: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT) || 5432,
-    username: process.env.DB_USERNAME || 'student',
-    password: process.env.DB_PASSWORD || 'student',
-    databaseName: process.env.DB_NAME || 'nest_project',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: parseInt(process.env.POSTGRES_PORT) || 5432,
+    username: process.env.POSTGRES_USER || 'student',
+    password: process.env.POSTGRES_PASSWORD || 'student',
+    databaseName: process.env.POSTGRES_DB || 'kupipodariday',
   },
   JWT_Secret: process.env.JWT_SECRET || 'anotherSecretKey',
 });
